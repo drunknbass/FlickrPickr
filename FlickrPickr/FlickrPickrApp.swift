d8edc249bd650ea78696daaab4db2543ab51ac47
Aaron Alexander <drunknbass@me.com>
2024-09-16 12:33:25 -0700

    Initial Commit

---
+++ FlickrPickr/FlickrPickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrPickrApp.swift
+//  FlickrPickr
+//
+//  Created by Aaron Alexander on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrPickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
